ee8d66471c0c0d58a4d9a24d9032e846fb0e1b1c
raihanadhitya23 <raihanadhitya23@gmail.com>
2025-04-28 12:02:47 +0700

    enable static

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,36 @@
-import adapter from '@sveltejs/adapter-auto';
-import { mdsvex } from 'mdsvex';
+// import adapter from '@sveltejs/adapter-auto';
+// import { mdsvex } from 'mdsvex';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   extensions: ['.svelte', '.md', '.svx'],
+//   kit: {
+//     adapter: adapter(),
+//   },
+//   preprocess: [
+//     mdsvex({
+//       extensions: ['.md', '.svx'],
+//     }),
+//   ],
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.md', '.svx'],
-  kit: {
-    adapter: adapter(),
-  },
-  preprocess: [
-    mdsvex({
-      extensions: ['.md', '.svx'],
-    }),
-  ],
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter()
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/Unremarkable-Echoes' : '',
+        }
+    }
 };
 
 export default config;
